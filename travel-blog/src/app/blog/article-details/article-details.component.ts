669c735ca5b4a8a1a2b08a52f4caad74a9a82d48
Burca Lidia-Gabriela <lidia.burca@centric.eu>
2023-07-11 18:35:47 +0300

    Add article-details

--- travel-blog/src/app/blog/article-details/article-details.component.ts
+++ travel-blog/src/app/blog/article-details/article-details.component.ts
@@ -3,8 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-article-details',
   templateUrl: './article-details.component.html',
-  styleUrls: ['./article-details.component.scss']
+  styleUrls: ['./article-details.component.scss'],
 })
 export class ArticleDetailsComponent {
-
+  date: Date = new Date();
 }
