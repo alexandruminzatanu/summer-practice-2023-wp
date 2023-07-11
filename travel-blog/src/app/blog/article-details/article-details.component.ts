bf01c5affa574dd96729db5c63bacd02301de755
Alex Minzatanu <alexandru.minzatanu@gmail.com>
2023-07-11 19:28:27 +0300

    routing angular

--- travel-blog/src/app/blog/article-details/article-details.component.ts
+++ travel-blog/src/app/blog/article-details/article-details.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-article-details',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ArticleDetailsComponent {
   date: Date = new Date();
+  constructor(private activatedRoute: ActivatedRoute) {
+    this.activatedRoute.paramMap.subscribe((data) => {
+      console.log(data);
+    });
+  }
+
 }
--- travel-blog/src/app/blog/blog-routing.module.ts
+++ travel-blog/src/app/blog/blog-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ArticleListItemComponent } from './article/article-list-item/article-list-item.component';
+import { ArticleDetailsComponent } from './article-details/article-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'article-list',
+  component: ArticleListItemComponent
+},
+{
+  path: 'article-details/:id',
+  component: ArticleDetailsComponent
+}, {
+  path: '**',
+  redirectTo: 'article-list'
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
