c4348e5597bcd9ea8128815f5747ad721b7187d3
Alexandra Mosoiu <alexandramosoiu19@gmail.com>
2023-07-18 09:59:03 +0300

    added rerouting in guard

--- travel-blog/src/app/guards/allowed-country.guard.ts
+++ travel-blog/src/app/guards/allowed-country.guard.ts
@@ -1,11 +1,14 @@
-import { CanActivateFn } from '@angular/router';
+import { inject } from '@angular/core';
+import { CanActivateFn, Router } from '@angular/router';
 
 const bannedCountries = ['Germany', 'France'];
 
 export const allowedCountryGuard: CanActivateFn = (route, state) => {
   const country = String(route.paramMap.get('title'));
   if (bannedCountries.includes(country)) {
-    alert("You don't have permision to access this page");
+    // alert("You don't have permision to access this page"); // daca vreti sa afisati doar un mesaj de alerta pt utilizator fara redirectionare
+    const router = inject(Router);
+    router.navigate(['']);
     return false;
   }
   return true;
