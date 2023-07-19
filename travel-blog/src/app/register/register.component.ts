1381535669a016cdb24bc3c9d576c09fdaaa5572
Tudor Tomescu <46830306+Herastis@users.noreply.github.com>
2023-07-19 10:54:22 +0300

    Add ratingRange custom validator

--- travel-blog/src/app/register/register.component.ts
+++ travel-blog/src/app/register/register.component.ts
@@ -1,7 +1,11 @@
 import { Component } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup, Validators } from '@angular/forms';
 
-//LIVE
+function ratingRange(c: AbstractControl): { [key: string]: boolean } | null {
+  if (c.value  && (isNaN(c.value) || c.value < 1 || c.value > 5 ))
+    return { 'range': true };
+  return null;
+}
 
 @Component({
   selector: 'app-register',
@@ -15,22 +19,21 @@ export class RegisterComponent {
   ngOnInit() {
     this.formGroup = new FormGroup({
       // (Task) Add 10 characters maximum length validator for the first name field
-      firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+      firstName: new FormControl('', [Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
       lastName: new FormControl('', Validators.required),
       phone: new FormControl('', Validators.required),
       email: new FormControl(''),
       sendCatalog: new FormControl(),
+      rating: new FormControl('', ratingRange),
       
       // (Task) Create an address formControl that is required in the formGroup
-
-      //
+      address: new FormControl('', [Validators.required, Validators.minLength(5)])
     })
 
     this.formGroup.valueChanges.subscribe((value) => {
       // console.log("Form model: ", this.formGroup);
-      // console.log('Erorrs:', this.formGroup.get('firstName')?.errors);
-      // console.log('New value:', value);
-      // Perform additional actions with the new value
+      // console.log('First name erorrs:', this.formGroup.get('firstName')?.errors);
+      console.log('New value:', value);
     });
   }
 
@@ -53,27 +56,29 @@ export class RegisterComponent {
   }
 
   setOrPatchValue(): void {
-    // this.formGroup.setValue({
-    //   firstName: 'firstName',
-    //   lastName: 'lastName',
-    //   phone:'phone',
-    //   email: 'email',
-    //   sendCatalog: 'sendCatalog',
-    //   //rating: 2
-    // })
+    this.formGroup.setValue({
+      firstName: 'firstName',
+      lastName: 'lastName',
+      phone:'phone',
+      email: 'email',
+      sendCatalog: 'sendCatalog',
+      rating: 2,
+      address: 'asd'
+    })
 
     // this.formGroup.patchValue({
     //   firstName: "First Name",
     //   lastName: "Last Name"
     // })
 
-    this.formGroup.patchValue({
-      firstName: 'firstName',
-      lastName: 'lastName',
-      phone:'phone',
-      email: 'email',
-      sendCatalog: 'sendCatalog',
-      rating: 2
-    })
+    // this.formGroup.patchValue({
+    //   firstName: 'firstName',
+    //   lastName: 'lastName',
+    //   phone:'phone',
+    //   email: 'email',
+    //   sendCatalog: 'sendCatalog',
+    //   rating: 2,
+    //   address: 'asd'
+    // })
   }
 }
